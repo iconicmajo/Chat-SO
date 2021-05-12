121bc4feed48d66de49cad2b2c2e000770bee925
juanferdeleon <dele17822@uvg.edu.gt>
2021-05-11 20:20:37 -0600

    Server

--- server.c
+++ server.c
@@ -68,7 +68,7 @@ void queue_add(client_t *cl){
     for(int i=0; i<MAX_CLIENTS; ++i){
         if(!clients[i]){
             clients[i] = cl;
-            printf("Prueba: %s\n", clients[i]->name);
+            printf("Prueba: %s\n", *clients[i]->name);
 
             break;
         }
