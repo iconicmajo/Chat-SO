e77983f4801bf284364af4e2fe4f4c73ab871a38
mcastro99 <cas181202@uvg.edu.gt>
2021-05-21 12:50:16 -0600

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -115,7 +115,7 @@ void *recv_msg_handler(void *ptr){
     }
 }
 
-int main(int argc, char **argv){
+int main(int argc, char argv){
     if(argc != 2){
         printf("Usage: %s <port>\n", argv[0]);
         return EXIT_FAILURE;
